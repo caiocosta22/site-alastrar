d11ea5f3258cae1d0b54b8f2eaa850fc2e919e45
caiocosta22 <caiocosta22@gmail.com>
2024-01-09 09:56:02 -0300

    adicionado iframe do youtube

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
 import apresentacao from "src/components/apresentacao.vue";
 import trabalhos from "src/components/trabalhos.vue";
 import institucional from "src/components/institucional.vue";
+import showcase from "src/components/showcase.vue";
 </script>
 
 <template lang="pug">
@@ -9,6 +10,7 @@ q-page-container
   apresentacao
   institucional
   trabalhos
+  showcase
 </template>
 
 <style scoped>
